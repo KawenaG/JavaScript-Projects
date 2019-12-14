60d26e3cf7f2a23fd5d083abc7fdc869c6ec0001
KawenaG <45134325+KawenaG@users.noreply.github.com>
2019-12-13 17:26:27 -0800

    Example form

--- JS/JavaScript_1.js
+++ JS/JavaScript_1.js
@@ -1,4 +1,9 @@
-function Hello_World_Function() {
-    var A = document.getElementsByClassName("Click")
-    A[0].innerHTML = "The text has changed!";
-}
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+// Create gradient
+var grd = ctx.createLinearGradient(0,0,200,0);
+grd.addColorStop(0,"red");
+grd.addColorStop(1,"white");
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(10,10,150,80);
