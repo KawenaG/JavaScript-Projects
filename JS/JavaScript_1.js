5f5f8ba4e2ce97aa8dbce2ab377a5cab0a874629
KawenaG <45134325+KawenaG@users.noreply.github.com>
2019-12-13 16:45:12 -0800

    Get Element by Class Name Method

--- JS/JavaScript_1.js
+++ JS/JavaScript_1.js
@@ -1,25 +1,4 @@
-function Guessing_function() {
-    var text;
-    var myNumber = document.getElementById("myInput").value;
-
-    switch(myNumber) {
-        case "one":
-        text = "Close, but that's not quite it.";
-        break;
-        case "two":
-        text = "Correct! How'd you know?";
-        break;
-        case "three":
-        text = "Close, but that's not quite it.";
-        break;
-        case "four":
-        text = "nope, that's too high.";
-        break;
-        case "five":
-        text = "nope, that's too high.";
-        break;
-        default:
-        text = "Please enter a valid number 1 through 5."
-    }
-    document.getElementById("guess").innerHTML = text;
+function Hello_World_Function() {
+    var A = document.getElementsByClassName("Click")
+    A[0].innerHTML = "The text has changed!";
 }
